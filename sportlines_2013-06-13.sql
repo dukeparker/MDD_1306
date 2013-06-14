2c6749deed50689ac3f75a161a53e008bc557713
Duke Parker <dukeparker@gmail.com>
2013-06-13 23:49:08 -0500

    fuelPHP & Bootstrap

---
+++ sportlines_2013-06-13.sql
@@ -0,0 +1,115 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.29)
+# Database: sportlines
+# Generation Time: 2013-06-13 23:27:17 -0500
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table comments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `story_id` int(11) NOT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table joins
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `joins`;
+
+CREATE TABLE `joins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `joins` WRITE;
+/*!40000 ALTER TABLE `joins` DISABLE KEYS */;
+
+INSERT INTO `joins` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`)
+VALUES
+	(1,'Duke','dukeparker@gmail.com','password',1371098800,1371098800);
+
+/*!40000 ALTER TABLE `joins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table migration
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `migration`;
+
+CREATE TABLE `migration` (
+  `type` varchar(25) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `migration` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `migration` WRITE;
+/*!40000 ALTER TABLE `migration` DISABLE KEYS */;
+
+INSERT INTO `migration` (`type`, `name`, `migration`)
+VALUES
+	('app','default','001_create_joins'),
+	('app','default','002_create_stories'),
+	('app','default','003_create_comments');
+
+/*!40000 ALTER TABLE `migration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table stories
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `stories`;
+
+CREATE TABLE `stories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `headline` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `byline` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
